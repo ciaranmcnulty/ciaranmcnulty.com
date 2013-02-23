2651ae7e5f6efa2517b420993dd1c4c26506411b
Ciaran McNulty <mail@ciaranmcnulty.com>
2013-02-23 18:00:58 +0000

    Adding mobile viewport

--- includes/head-boilerplate.php
+++ includes/head-boilerplate.php
@@ -1,4 +1,5 @@
 <!--[if lt IE 9]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
+<meta name="viewport" content="width=device-width" />
 <link rel="stylesheet" href="/styles.css?v=2" />
