68998c74fe3fcdecb987f33b95aaca952c64a30e
Ciaran McNulty <mail@ciaranmcnulty.com>
2013-02-24 12:32:56 +0000

    Refactoring the styling and improving the rendering in legacy browsers

--- includes/head-boilerplate.php
+++ includes/head-boilerplate.php
@@ -2,4 +2,7 @@
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 <meta name="viewport" content="width=device-width" />
-<link rel="stylesheet" href="/styles.css?v=4" />
+<link rel="stylesheet" href="/main.css?v=1" />
+<!--[if gt IE 7]><!-->
+<link rel="stylesheet" href="/layout.css?v=1" />
+<!--<![endif]-->
