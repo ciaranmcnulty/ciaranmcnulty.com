949dea4584ccefbd8da8f5e063a664ded650f3ad
Ciaran McNulty <mail@ciaranmcnulty.com>
2015-07-05 19:11:07 +0100

    Don't publish to remotes automatically

--- publish.sh
+++ publish.sh
@@ -4,5 +4,4 @@ git checkout -b master
 git add -f output_prod
 git commit -m "Publish site"
 git filter-branch --subdirectory-filter output_prod/ -f
-git push origin -f master
-git push heroku -f master
+
