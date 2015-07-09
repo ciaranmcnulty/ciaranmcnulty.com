f8fbbe8f30488f8eda94df32dd88b693a6daec49
Ciaran McNulty <mail@ciaranmcnulty.com>
2015-07-09 20:26:02 +0100

    Try to build in travis

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
+if [ "$#" -ne 1 ]; then
+    echo "Provide a branch name only"
+    exit;
+fi
 ./bin/sculpin generate --env=prod
-git branch -D master
-git checkout -b master
+git branch -D $1
+git checkout -b $1
 git add -f output_prod
 git commit -m "Publish site"
 git filter-branch --subdirectory-filter output_prod/ -f
